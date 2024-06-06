c347dda9cca387fbdfc2995cb282667fec6c2cab
Vihan Perera <vihanpereraux@gmail.com>
2024-06-06 23:06:14 +0100

    add functions

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -14,9 +14,19 @@ for (let index = 0; index < leftItemWrappers.length; index++) {
                 for (let index = 0; index < leftHgSplitPanelSet_1.length; index++) {
                     switch (index + 1) {
                         case 1:
-                            leftHgSplitPanelSet_1[index].classList.remove('reverse_animate_block_one');
-                            leftHgSplitPanelSet_1[index].style.width = '80%';
-                            leftHgSplitPanelSet_1[index].classList.add('animate_block_one');
+                            dumyFuncOne('reverse_animate_block_one', '80%', 'animate_block_one', index);
+                            break;
+
+                        case 2:
+                            dumyFuncOne('reverse_animate_block_two', '80%', 'animate_block_two', index);
+                            break;
+
+                        case 3:
+                            dumyFuncOne('reverse_animate_block_three', '70%', 'animate_block_three', index);
+                            break;
+
+                        case 4:
+                            dumyFuncOne('reverse_animate_block_four', '60%', 'animate_block_four', index);
                             break;
 
                         default:
@@ -40,9 +50,19 @@ for (let index = 0; index < leftItemWrappers.length; index++) {
                 for (let index = 0; index < leftHgSplitPanelSet_1.length; index++) {
                     switch (index + 1) {
                         case 1:
-                            leftHgSplitPanelSet_1[index].classList.remove('animate_block_one');
-                            leftHgSplitPanelSet_1[index].style.width = '0%';
-                            leftHgSplitPanelSet_1[index].classList.add('reverse_animate_block_one');
+                            dumyFunc('animate_block_one', 'reverse_animate_block_one', index);
+                            break;
+
+                        case 2:
+                            dumyFunc('animate_block_two', 'reverse_animate_block_two', index);
+                            break;
+
+                        case 3:
+                            dumyFunc('animate_block_three', 'reverse_animate_block_three', index);
+                            break;
+
+                        case 4:
+                            dumyFunc('animate_block_four', 'reverse_animate_block_four', index);
                             break;
 
                         default:
@@ -55,4 +75,17 @@ for (let index = 0; index < leftItemWrappers.length; index++) {
                 break;
         }
     });
+}
+
+
+function dumyFunc(animationNameOne, animationNameTwo, index) {
+    leftHgSplitPanelSet_1[index].classList.remove(animationNameOne);
+    leftHgSplitPanelSet_1[index].style.width = '0%';
+    leftHgSplitPanelSet_1[index].classList.add(animationNameTwo);
+}
+
+function dumyFuncOne(animationNameOne, width, animationNameTwo, index) {
+    leftHgSplitPanelSet_1[index].classList.remove(animationNameOne);
+    leftHgSplitPanelSet_1[index].style.width = width;
+    leftHgSplitPanelSet_1[index].classList.add(animationNameTwo);
 }
